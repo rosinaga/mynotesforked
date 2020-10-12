add642b775408676b2445f126d2d8d7a24564aae
Jukka-Pekka <jpkerkkanen@gmail.com>
2020-10-13 01:35:09 +0300

    Working on templates and static files

--- notes/views.py
+++ notes/views.py
@@ -3,4 +3,4 @@ from .models import Note, Topic
 
 def home(request):
     alltopics = Topic.objects.all()
-    return render(request, 'home.html', {'topics_for_home': alltopics})
+    return render(request, 'home.html',{'topics_for_home': alltopics})
