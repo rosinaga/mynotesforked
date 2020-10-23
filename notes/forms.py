75aa26d8cfc171e1e940440d49935d3ac5dac0db
Jukka-Pekka <jpkerkkanen@gmail.com>
2020-10-23 22:19:22 +0300

    Form error messages made nicer

--- notes/forms.py
+++ notes/forms.py
@@ -2,7 +2,11 @@ from django import forms
 from notes.models import Topic
 
 class NewTopicForm(forms.ModelForm):
-    description = forms.CharField(widget=forms.Textarea(), max_length=500)
+    description = forms.CharField(
+        widget=forms.Textarea(),
+        max_length=500,
+        help_text='The max length of the text is 500 characters.'
+    )
 
     class Meta:
         model = Topic
