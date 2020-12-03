3bb46e1738bfc77e23ede28e6ad77cff5a9de6f2
Jukka-Pekka Kerkkänen <kerkkaju@teacher.eursc.eu>
2020-12-03 17:23:31 +0100

    Edit topic almost there

--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import timezone
 from django.contrib.auth.models import User
-from notes.forms import NewTopicForm
+from notes.forms import NewTopicForm, EditTopicForm
 from notes.models import Note, Topic
 
 def home(request):
@@ -24,20 +25,22 @@ def topic_new(request):
     else:
         return redirect('login')
 
-def topic_edit(request):
-    user = request.user # Gives you the currently logged-in user.
-    topic = request.topic
-    # Only topic owner can edit the topic:
-    if user.is_authenticated and user == topic.owner:
-        if request.method == 'POST':
-            form = NewTopicForm(request.POST)
-            if form.is_valid():
-                topic = form.save(commit=False)
-                topic.owner = user
-                topic.save()
-                return redirect('url_topics')
+# Parameter "topic_id" is a keyword argument (kwarg) referring to the primary key
+# (pk) of the object. We named "topic_id" in urls.py.
+def topic_edit(request, topic_id):
+    user = request.user
+    if user.is_authenticated:
+        topic = get_object_or_404(Topic, pk=topic_id)
+        if topic.owner != user: # Important!
+            return HttpResponseForbidden()
         else:
-            form = NewTopicForm()
-        return render(request, 'topic_new.html', {'topic_form': form})
+            if request.method == 'POST':
+                form = EditTopicForm(request.POST)
+                if form.is_valid():
+                    topic = form.save()
+                    return redirect('url_topics')
+            else:
+                form = EditTopicForm()
+        return render(request, 'topic_edit.html', {'topic_form': form})
     else:
         return redirect('login')
